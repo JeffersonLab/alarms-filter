515e1fe3e37caf8860a8bbc5867f68869ac578e1
ryans <ryans@jlab.org>
2021-02-25 17:52:09 -0500

    Initial setup of client command scripts

---
+++ scripts/set-filter.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+[ -z "$BOOTSTRAP_SERVERS" ] && echo "BOOTSTRAP_SERVERS environment required" && exit 1;
+
+# Grab first SERVER from SERVERS CSV env
+IFS=','
+read -ra tmpArray <<< "$BOOTSTRAP_SERVERS"
+
+BOOTSTRAP_SERVER=${tmpArray[0]}
+
+help=$'Usage:\n'
+help+="  Set:   $0 [-n] name [-e] expression"
+help+=$'\n'
+help+="  Unset: $0 [-n] name -u"
+
+while getopts ":c:t:m:o:u" opt; do
+  case ${opt} in
+    u )
+      unset=true
+      ;;
+    n )
+      name=$OPTARG
+      ;;
+    e )
+      expression=$OPTARG
+      ;;
+    \? ) echo "$help"
+      ;;
+  esac
+done
+
+if ((OPTIND == 1))
+then
+    echo "$help"
+    exit
+fi
+
+if [ ! "$channel" ]
+then
+  echo "$help"
+  exit;
+fi
+
+if [ "$unset" ]
+then
+  # kafka-console-producer can't write tombstone (null) messages!
+  # Hack - we will just compile and run tiny Java program then!
+
+CWD=$(readlink -f "$(dirname "$0")")
+
+APP_HOME=$CWD/..
+
+FILTER_JAR=`ls $APP_HOME/lib/alarms-filter*`
+CLIENTS_JAR=`ls $APP_HOME/libs/kafka-clients-*`
+JACK_CORE=`ls $APP_HOME/libs/jackson-core-*`
+JACK_BIND=`ls $APP_HOME/libs/jackson-databind-*`
+JACK_ANN=`ls $APP_HOME/libs/jackson-annotations-*`
+SLF4J_API=`ls $APP_HOME/libs/slf4j-api-*`
+SLF4J_IMP=`ls $APP_HOME/libs/slf4j-log4j*`
+LOG4J_IMP=`ls $APP_HOME/libs/log4j-*`
+LOG4J_CONF=$CWD
+
+RUN_CP="$FILTER_JAR:$CLIENTS_JAR:$SLF4J_API:$SLF4J_IMP:$LOG4J_IMP:$LOG4J_CONF:$JACK_CORE:$JACK_BIND:$JACK_ANN"
+
+java -cp $RUN_CP CommandProducer $BOOTSTRAP_SERVERS filter-commands $name $expression
